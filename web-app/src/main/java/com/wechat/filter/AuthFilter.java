cc7d6be40c8a2f7022ae58cea3cc5f983a819f02
unknown <gucheng_en@icloud.com>
2017-03-29 23:17:20 +0800

    frame 原型出来了

---
+++ web-app/src/main/java/com/wechat/filter/AuthFilter.java
@@ -0,0 +1,25 @@
+package com.wechat.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by chengen.gu on 2017/3/28.
+ */
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ web-service/src/main/java/com/wechat/Servermain.java
@@ -0,0 +1,17 @@
+package com.wechat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by chengen.gu on 2017/3/29.
+ */
+public class Servermain {
+
+    private static Logger logger = LoggerFactory.getLogger(Servermain.class);
+
+    public static void main(String[] args) {
+
+        logger.warn("servermain start");
+    }
+}
